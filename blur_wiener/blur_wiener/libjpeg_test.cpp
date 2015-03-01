22379709898be5fe5adf76578cff97e93c3b8eee
Alexander Nikolaev <ainikolaev@gmail.com>
2015-03-01 15:30:59 +0300

    LibJpeg half way working.
    All the code is there but it doesn’t save the file now.

--- blur_wiener/blur_wiener/libjpeg_test.cpp
+++ blur_wiener/blur_wiener/libjpeg_test.cpp
@@ -1,89 +1,8 @@
-/*
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <iostream>
-
-using namespace cv;
-using namespace std;
-
-int main( int argc, char** argv )
-{
-    if( argc != 2)
-    {
-     cout <<" Usage: display_image ImageToLoadAndDisplay" << endl;
-     return -1;
-    }
-
-    Mat image;
-    image = imread(argv[1], IMREAD_COLOR); // Read the file
-
-    if(! image.data ) // Check for invalid input
-    {
-        cout << "Could not open or find the image" << std::endl ;
-        return -1;
-    }
-
-	Mat blurred_image;
-	blurred_image = 
-
-
-    namedWindow( "Display window", WINDOW_AUTOSIZE ); // Create a window for display.
-    imshow( "Display window", image ); // Show our image inside it.
-
-    waitKey(0); // Wait for a keystroke in the window
-    return 0;
-}
-*/
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include <iostream>
-#include "jpeglib.h"
-
-
-#include "GaussianDegrader.h"
-#include "MathTools.h"
-
-
-using namespace std;
-using namespace cv;
-
-
-void testWiener(char** argv)
-{
-	int const MAX_KERNEL_LENGTH = 31;
-	char buffer[MAX_KERNEL_LENGTH];
-
-	Mat src; Mat dst;
-
-	string output_location;
-
-	/// Load the source image
-	int isColor = 0;
-	src = imread(argv[1], isColor); // Read the file
-
-	dst = src.clone();
-
-	/// Applying Gaussian blur
-	GaussianDegrader degrader(3);
-	degrader.process(src, dst);
-
-	imwrite("blur.bmp", dst);
-
-	cv::Mat kernelImg;
-	cv::Mat kernelScaled;
-	normalize(degrader.kernel, kernelScaled, 0, 255, CV_MINMAX);
-	
-	kernelScaled.convertTo(kernelImg, CV_8UC1);
-	imwrite("kernel.bmp", kernelImg);
-	double k = 0.01;
-	Mat deblur;
-	MathTools::wienerFilter(dst, degrader.kernel, k, deblur);
-	std::cout << deblur.type() << " " << CV_8UC1 << std::endl;
-	imwrite("deblur.bmp", deblur);
-}
-
-/* This some code to test that libjpeg is doing alright and that the Linker is also sane.*/
-
+//http://www.aaronmr.com/en/2010/03/test/
+//This is an example taken from the link above.
+#include <stdio.h>
+#include <jpeglib.h>
+#include <stdlib.h>
 
 /* we will be using this uninitialized pointer later to store raw, uncompressd image */
 unsigned char *raw_image = NULL;
@@ -102,8 +21,6 @@ int color_space = JCS_RGB; /* or JCS_GRAYSCALE for grayscale images */
 * \param *filename char string specifying the file name to read from
 *
 */
-
-
 
 int read_jpeg_file(char *filename)
 {
@@ -215,85 +132,8 @@ int write_jpeg_file(char *filename)
 	return 1;
 }
 
-
-
-/**
- * function main
- */
- int main( int argc, char** argv )
- {
-
-	testWiener(argv);
-//	return 0; Do this and nothing after will happen 
-
-	int const MAX_KERNEL_LENGTH = 31;
-	char buffer [MAX_KERNEL_LENGTH];
-	Mat src; Mat dst;
-
-	string output_location;
-
-	 if( argc != 2)
-    {
-     cout <<" Usage: blur_wiener ImageToLoadAndDisplay" << endl;
-     return -1;
-    }
-
-   /// Load the source image
-   src = imread(argv[1], IMREAD_COLOR); // Read the file
-
-   
-   dst = src.clone();
-   
-    /// Applying Gaussian blur
-    for ( int i = 1; i < MAX_KERNEL_LENGTH; i = i + 2 )
-    {
-		GaussianDegrader degrader(i);
-		degrader.process(src, dst);
-		/*
-		//Genrating Gaussian kernel by hand
-		Mat kernel(i, i, CV_32F);//getGaussianKernel(i*6+1, i);
-		double center = (double) i / 2.0 - 0.5;
-		double sigma = i / 4.0;
-		for (int rowInd = 0; rowInd < kernel.rows; rowInd++)
-		{
-			for (int colInd = 0; colInd < kernel.cols; colInd++)
-			{
-				double sx = rowInd - center;
-				double sy = colInd - center;
-				kernel.at<float>(rowInd, colInd) = pow(2.7181, (-sx*sx - sy*sy) / 2 / sigma / sigma); 
-			}
-		}
-		Scalar chSum = sum(kernel);
-		double kSum = chSum[0];
-		std::cout << kSum << std::endl;
-		kernel = kernel / kSum;
-
-		
-		
-		degradeImage(src, dst, kernel);
-		*/
-
-		//GaussianBlur( src, dst, Size( i, i ), 0, 0 );
-
-		//Save the resulting image. Need to construct the path.
-
-		//char *intStr = itoa(i, buffer, 10);
-		//string i_s = string(intStr);
-		// I'm not using GCC, to this is cool.
-		string i_s = to_string(i);
-		string argvs = string(argv[1]);
-		string extention = ".tiff";
-		output_location = "./";
-		output_location = "./" + i_s + ".tiff";
-		//output_location = output_location.append (i_s, argvs);
-
-		imwrite( output_location, dst );
-	}
-	/* This some code to test that libjpeg is doing alright.*/
-	//j_compress_ptr cinfo; //This one is OK.
-	//jpeg_abort_compress(cinfo); //This one is not if Linker is crazy.
-
-	/*Now, actually doing something.*/
+int main()
+{
 	char *infilename = "test.jpg", *outfilename = "test_out.jpg";
 
 	/* Try opening a jpeg*/
@@ -303,8 +143,6 @@ int write_jpeg_file(char *filename)
 		if (write_jpeg_file(outfilename) < 0) return -1;
 	}
 	else return -1;
-
-
-     return 0;
- }
+	return 0;
+}
 
